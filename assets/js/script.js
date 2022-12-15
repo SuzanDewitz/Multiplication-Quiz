d5ba296e863d29dd17f7ab93671ff73877107cdc
Suzan Dewitz <suzanyaa@yahoo.com>
2022-12-15 14:17:57 +0000

    Add meta data to head and connect css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,100 @@
-const questionElement = document.getElementById("question");
-let num1 = Math.floor(Math.random() * 10);
-let num2 = Math.floor(Math.random() * 10);
-let correctAnswer = num1 * num2;
-  
-questionElement.innerText = `What is ${num1} Multiply by ${num2}?`;
-  
-const form = document.getElementById('form');
-const input = document.getElementById('inp');
-let scoreElement = document.getElementById('score');
-  
-let score = Number(localStorage.getItem("score"));
-if(!score) {
-    score = 0;
+const quizData = [
+    {
+        question: "What is the most used programming language in 2019?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "Who is the President of US?",
+        a: "Florin Pop",
+        b: "Donald Trump",
+        c: "Ivan Saldano",
+        d: "Mihai Andrei",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "Jason Object Notation",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 }
-  
-scoreElement.textContent = `score : ${score}`;
-  
-form.addEventListener('submit',function() {
-    let userAnswer = +input.value;
-    if(correctAnswer === userAnswer) {
-        score++;
-        updateScore();
-    }
-    else {
-        score--;
-        updateScore();
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
     }
 });
-  
-function updateScore() {
-    localStorage.setItem("score",String(score));
-}
-  
-// Clear Local Storage 
-// localStorage.removeItem("score");
